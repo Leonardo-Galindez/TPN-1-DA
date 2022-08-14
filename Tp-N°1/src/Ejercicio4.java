70f7721776a9a819723c834e221b2c3b3b9edbcc
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2022-08-14 19:18:49 -0300

    ejercicio 4 terminado

--- Tp-N°1/src/Ejercicio4.java
+++ Tp-N°1/src/Ejercicio4.java
@@ -1,90 +1,38 @@
-
 import java.util.Scanner;
 public class Ejercicio4 {
     public static void main(String[]args){
-        String frase,palabra,bin;
-        int mayor,j,cantPala,ocurr,i;
-        i=0;
-        cantPala=1;
-        ocurr=0;
-        bin="";
-        j=0;
-        mayor=0;
-        boolean valor;
-        valor=false;
-        palabra="";
-
         Scanner sc=new Scanner(System.in);
-
-        System.out.println("Ingrese frase");
-        frase=sc.nextLine();
         int longitud;
-        longitud=frase.length();
-        cantPala=CantOcurrencias(frase);
-
-        while(i<longitud && ocurr<longitud){
-            if(frase.charAt(ocurr)==' '){
-                palabra=subCadena(frase, ocurr+j-ocurr,ocurr-1 );
-                System.out.println(palabra);
-                j=palabra.length();
-                if(mayor<j){
-                   mayor=j;
-                }
-                ocurr=j+2;
-            }
-            ocurr++;
-            i++;
-        }
-
-           
-        
-
-
-
-
-       
-    }
-     /*System.out.println(palabra);
-        j=palabra.length();
+        String frase,palabra;
         palabra="";
-        /*for (int i = j+1; i < longitud; i++) {
-            palabra = palabra + frase.charAt(i);
-        }     
-        System.out.println(palabra);*/
-
-    //modulo para contar la cantidada de palabras de la frase
-    public static int CantOcurrencias(String cadena){
-        int palabras,longitud;
-        palabras=1;
+        System.out.println("Ingrese la frase");
+        frase = sc.nextLine();
+        palabra=WordBig(frase);
+        longitud=frase.length();
+        System.out.println("La palabra mas larga es :"+palabra);
+    }
+    
+    //modulo para 
+    public static String WordBig(String cadena){
+        String nombre,palabra;
+        int i,longitud,mayor;
+        char caracter;
+        palabra ="";
+        mayor=0;
+        nombre="";
         longitud=cadena.length();
-        for(int i=0; i<longitud;i++){
-            if(cadena.charAt(i)==' '){
-                palabras++;
-            } 
-        }
-        return palabras;
-    }
-
-    public static String subCadena(String b, int pos1, int pos2) {
-        // 3: Módulo para obtener una subcadena
-        // b es la cadena de caracteres que ingresa el usuario
-        // pos1 es la primera posicion 
-        // pos2 es la segunda posicion
-        // retorna la subcadena que va de pos1 hasta pos2 inclusive
-        int i, cant;
-        String bin;
-        bin = "";
-        cant = b.length();
-        if (pos1 >= 0 && pos1 < cant && pos2 >= 0 && pos2 < cant && pos1<=pos2) {
-            // Validación para que las posiciones no sean menores a 0 ni mayores a cantidad de caracteres
-            // pos1 siempre tiene que ser menor que pos2
-            for (i = pos1; i <= pos2; i++) {
-                bin = bin + b.charAt(i);
+        for(i=0;i<longitud;i++){
+            caracter=cadena.charAt(i);
+            if(caracter==' '){
+                nombre="";
+            }else{
+                nombre= nombre+caracter;
             }
-        } else {
-            bin = "ERROR.";
+            if(nombre.length()>mayor){
+                mayor=nombre.length();
+                palabra=nombre;
+            }
         }
-        return bin;
+        return palabra;
     }
-     
 }
