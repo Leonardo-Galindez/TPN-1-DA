0ae641768619ed5ad224d71e2eca925a0270a811
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2022-08-17 16:23:28 -0300

    CAMBIOS

--- Tp-N°1/src/App.java
+++ Tp-N°1/src/App.java
@@ -1,5 +1,17 @@
+import javax.swing.SpringLayout;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int i=0, n=5,suma=0,suma2;
+        for(i=1;i<=n;i++){
+            suma2=0;
+            for(int j=1;j<=i;j++){
+                suma2=suma2+j;
+
+            }
+            suma=suma+suma2;
+            
+        }
+        System.out.println(suma);
     }
 }
